52809e09008aeee9902e9d509e7eab9993cb383e
KARTIKAY DALAL <2342246@swansea.ac.uk>
2026-02-28 14:06:51 +0000

    new changes in the number guessing

--- Numberguess.py
+++ Numberguess.py
@@ -3,23 +3,15 @@ import random
 secret = random.randint(1, 100)
 attempts = 0
 guessing = True
+print (secret)
 
 while guessing:
-    ask = input('try to guess the number ').strip()
-
-    if ask.isdigit():
-        ask = int(ask)
-
-        if ask == secret:
-            print('you are such a genius for that')
-            guessing = False
-
-        elif ask > secret:
-            print('too high')
-            continue
-
-        elif ask < secret:
-            print('too low')
-            continue
-    else:
-        print('numbers only')
+    ask=int(input('try your bestto guess the number'))
+    if ask ==secret:
+        print ('well done you got it genuis')
+    elif ask> secret:
+        print ('too high try agian ')    
+    elif ask<secret:
+        print('too low ')
+    else :
+        print('numbers only')        
