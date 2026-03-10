e25c66acf1c1f7481735c78e24d1332bb1e9c92b
KARTIKAY DALAL <2342246@swansea.ac.uk>
2026-03-10 13:34:17 +0000

    learned using the enumerate !! learned using the enumerate !!

--- To-dolist.py
+++ To-dolist.py
@@ -1,8 +1,11 @@
 to_do_list = [] # Stage 1 of the file , now we are going for level 2 where we gonna make proffesional 
+
+# now we are going to use enumerate 
+#
 listing = True
 
 while listing:
-    ask = input("Do you want to (A)dd a task, (V)iew tasks,(R)emove tasks or (Q)uit? ").capitalize().strip()
+    ask = input("Do you want to (A)dd a task, (V)iew tasks,(R)emove tasks or (Q)uit? new ").capitalize().strip()
     
     if ask == 'A':
         new_task = input("What task would you like to add? ")
@@ -10,16 +13,17 @@ while listing:
         print(f"Added: {new_task}")
 
     elif ask == 'V':
-        print('\n' + '='*10 + " TO-DO LIST " + '='*10)
-        for i in to_do_list:
-            print(i)
+        print('='*30,'to-do-list', '='*30)
+        for index,task in enumerate(to_do_list,start=1):
+            print(f"{index}.{task}")
     elif ask == 'Q':
         print("Closing program... Stay productive!")
         listing = False 
     elif ask =='R':
-        print('='*30,'To-DO-list','='*30)  
-        ask2=input('which task would you like to remove ')  
-        if ask2 in to_do_list :
-            to_do_list.remove(ask2)
-        else:
-            print('oops seems like this is not in your list ')    
+        print('='*10,'To-DO-list','='*10)  
+        for index,task in enumerate(to_do_list,start=1):
+            print(f"{index}.{task}")
+        ask2=int(input('which task would you like to remove '))  
+        removed_item = to_do_list.pop(ask2 - 1)
+        
+        print(f"Successfully removed: {removed_item}")  
